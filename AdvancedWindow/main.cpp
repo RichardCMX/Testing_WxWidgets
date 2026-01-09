c1b3e823e30aa715221aa374470ff2377f6bb752
RichardCMX <richardcm1157@gmail.com>
2026-01-09 16:03:39 -0600

    New file "Advanced Window" This file is a new project where I seeking deeper elements on the graphical intefaces. This window creates the first iteration of a form using singleLine and mutiLine text

---
+++ AdvancedWindow/main.cpp
@@ -0,0 +1,65 @@
+#include <wx/wx.h>
+#include <vector>
+
+class MyApp : public wxApp
+{
+public:
+	virtual bool OnInit();
+};
+
+wxIMPLEMENT_APP(MyApp);
+
+enum class InputType {
+	SingleLine,
+	MultiLine
+};
+
+class MyFrame : public wxFrame
+{
+public:
+	MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+};
+
+bool MyApp::OnInit()
+{
+	MyFrame* frame = new MyFrame("Advance Window Test", wxDefaultPosition, wxDefaultSize);
+	frame->Show(true);
+	return true;
+}
+
+/*
+wxFrame(parent, id, title, position, size)
+nullptr implies top-level window
+wxID_ANY implies wxWidgets assigns the ID
+*/
+MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size) : wxFrame(nullptr, wxID_ANY, title, pos, size)
+{
+	std::vector<std::pair<wxString, InputType>> form = {
+		{"Name", InputType::SingleLine},
+		{"Address", InputType::MultiLine},
+		{"Phone", InputType::SingleLine},
+		{"Email", InputType::SingleLine},
+		{"Notes", InputType::MultiLine}
+	};
+
+	const auto margin = FromDIP(10);
+	auto mainSizer = new wxBoxSizer(wxVERTICAL);
+	wxPanel* panel = new wxPanel(this, wxID_ANY);
+	auto sizer = new wxGridSizer(form.size(), 2, margin, margin);
+
+	for (const auto &[label, type]: form)
+	{
+		auto labelCtrl = new wxStaticText(panel, wxID_ANY, label);
+		sizer->Add(labelCtrl, 0, wxALIGN_CENTER_VERTICAL);
+
+		auto style = type == InputType::SingleLine ? 0 : wxTE_MULTILINE;
+		auto inputCtrl = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, style);
+
+		sizer->Add(inputCtrl, 1, wxEXPAND);
+	}
+	panel->SetSizer(sizer);
+
+	mainSizer->Add(panel, 1, wxEXPAND | wxALL, margin);
+
+	this->SetSizer(mainSizer);
+}
